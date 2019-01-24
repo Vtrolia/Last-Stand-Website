17319725408be22b58aa720023cf5cc661232fd8
Vtrolia <vtrolia@protonmail.com>
2019-01-23 19:53:04 -0600

    finished cloud interaction, data is sent correctly

--- laststand/api/views.py
+++ laststand/api/views.py
@@ -3,10 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.contrib.auth import authenticate
 from .models import Cloud
-from django.contrib.auth.models import User
 
 import helpers as h
-import requests
 
 
 # Create your views here.
@@ -20,11 +18,16 @@ def set_info(request, id):
     user = user.split("=")[1]
     password = password.split("=")[1]
 
-    owner = authenticate(request, username="Vinny", password="ShitBag1")
+    owner = authenticate(request, username=user, password=password)
 
     if owner == Cloud.objects.get(id=id).owner:
         cloud = Cloud.objects.get(id=id)
+        cloud.ip_address = request.META["HTTP_HOST"]
+        cloud.save()
         content = ""
         for user in cloud.users.all():
-            content += user.username + " " + user.password + "\n"
-        return HttpResponse("stfoo mga")
+            content += "content: " + user.username + " " + user.password + "\r\n"
+        return HttpResponse(content)
+    else:
+        return HttpResponse("contents: none failure" + "\r\n")
+
