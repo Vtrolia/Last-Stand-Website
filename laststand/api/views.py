af757fe2bdb59363d795a68fd4afb5260862fe1e
Vtrolia <vtrolia@protonmail.com>
2019-01-31 20:58:30 -0600

    added comments, yeah this is a lame one

--- laststand/api/views.py
+++ laststand/api/views.py
@@ -43,6 +43,7 @@ def set_info(request, name):
 def get_address(request, name):
     accessor = h.api_user_check(request)
 
+    # you have to be an allowed user to get a cloud's IP address from the server
     if accessor:
         resp = HttpResponse("content: " + Cloud.objects.get(id=name).ip_address + "\r\n")
     else:
@@ -53,20 +54,38 @@ def get_address(request, name):
 
 @csrf_exempt
 def get_ssl_cert(request, name):
+    # anyone can get the certificate for a server, just not the private key or any other data
     cert = Cloud.objects.get(id=name).ssl_cert
+
+    # delete an invalid cert
     if cert.date_expires > dt.date.today():
         return HttpResponse(cert.cacert)
     else:
         cert.delete()
         return HttpResponse("")
 
+
+@csrf_exempt
+def is_cert_valid(request, name):
+    # anyone can check whether or not a server has a valid ssl certificate or not
+    cert = Cloud.objects.get(id=name).ssl_cert
+    if cert.date_expires <= dt.date.today():
+        cert.delete()
+        return HttpResponse("content: expired")
+    else:
+        return HttpResponse("content: valid")
+
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def renew_cert(request, name):
     owner = h.api_user_check(request)
 
+    # only the owner is allowed to make a request for a new ssl certificate
     if owner:
         cert = Cloud.objects.get(id=name).ssl_cert
+
+        # create a completely new cert w/o deleting
         new_cert = h.generate_new_cert(ownername=owner.username, ownerpass=owner.password, name=name)
         cert.cacert = new_cert[0]
         cert.privkey = new_cert[1]
@@ -80,17 +99,25 @@ def renew_cert(request, name):
 
 @require_http_methods(["POST"])
 def submit_cloud(request):
+    # this is the initial creation of a cloud, so set the date
     created = dt.datetime.now()
     expires = created + relativedelta(month = 6)
     if not request.user.is_authenticated:
-        return HttpResponse("sanity check")
+        return HttpResponse("")
     else:
+
+        # has to be an authenticated user to create a cloud
         owner = request.user
+
+        # first create the ssl cert
         name = h.troliAlgorithm(owner.username, owner.password)
         auth_info = h.generate_new_cert(owner.username, owner.password, name)
         ssl = SSL.objects.create(cacert=auth_info[0], privkey=auth_info[1], date_created=created, date_expires=expires,
                                  created_by=owner, owned_by=owner)
         ssl.save()
+
+        # once the cert is saved, now connect it to the cloud being created. The user can give it a custom name, but
+        # its id is generated
         given_name = request.POST['name']
         ip_address = request.POST['ip']
         cloud = Cloud.objects.create(id=name, name=given_name, ip_address=ip_address, ssl_cert=ssl, owner=owner, status=0)
