bd924ef9372a7e2ab7279a7daa7932ec3139a275
vtrolia <vtrolia@protonmail.com>
2019-03-24 00:50:29 +0000

    added comments to cloud page and changed the response to only on success

--- laststand/static/scripts/cloudpage.js
+++ laststand/static/scripts/cloudpage.js
@@ -1,7 +1,15 @@
+/*
+ * When a user first selects to delete a cloud, prompt them to enter their password again for security reasons. 
+ * Making any changes to their account should first require reauthentication 
+ */
 function displayPasswordPrompt() {
+    
+    // make the radio button text more responsive
     document.getElementById("yes-remove").style.color = "black";
     document.getElementById("delete-cloud-confirmation").style.display = "inline-block";
     
+    // now, the button to submit a cloud deletion is disabled by default, again preventing the user from a
+    // hasty deletion until the password seems like the right one, then they are allowed to submit
     document.getElementById("delete-cloud-password").oninput = () => {
         var input = document.getElementById("delete-cloud-password").value;
         var button = document.getElementById("enter-delete-cloud");
@@ -15,7 +23,14 @@ function displayPasswordPrompt() {
     }
 }
 
+
+/*
+ * Once the user is authenticated, now they can delete the selected cloud. Sends a request back to the server
+ * and will display a message to the user based on the result of the attempted deletion
+ */
 function deleteThisCloud() {
+    
+    // delete a previous alert if they tried before
     var alert = document.getElementById("alerts");
     if (alert) {
         alert.parentNode.removeChild(alert);
@@ -23,6 +38,8 @@ function deleteThisCloud() {
     var name = document.getElementById("cloud-name").getAttribute("data-cloud");
     var password = document.getElementById("delete-cloud-password").value;
     
+    // send a JSON object back to the server with the name of the User's cloud and their
+    // password to be authenticated then deleted
     var request = new XMLHttpRequest();
     var data = {
         "name": name,
@@ -37,22 +54,29 @@ function deleteThisCloud() {
         var response = JSON.parse(request.responseText);
         var alert = document.createElement("div");
         
+        // create an alert div to tell the user what happened
         alert.setAttribute("id", "alerts");
         alert.setAttribute("class", "alert alert-" + response["flag"] + " cloud-alert");
         alert.innerHTML = response["body"];
         
         document.getElementById("cloud-page").append(alert);
+        
+        // remove the cloud that was just deleted from the display
+        var cloud = document.getElementById("cloud-options");
+        var clouds = cloud.querySelectorAll("li");
+
+        if (response["flag"] === 'success') {
+            for (let i = 0; i < clouds.length; i++)
+            {
+                 if (clouds.item(i).getAttribute("data-name") === name)
+                 {
+                     clouds.item(i).parentNode.removeChild(clouds.item(i));        
+                 }
+            }
+       }
+        
     }
     request.send(JSON.stringify(data));
     
-    var cloud = document.getElementById("cloud-options");
-    var clouds = cloud.querySelectorAll("li");
     
-    for (let i = 0; i < clouds.length; i++)
-    {
-         if (clouds.item(i).getAttribute("data-name") === name)
-         {
-             clouds.item(i).parentNode.removeChild(clouds.item(i));        
-         }
-    }
 }
