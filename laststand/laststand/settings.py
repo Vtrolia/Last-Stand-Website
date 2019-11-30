c5cf639c23eec2c72d779b345a8438319d85d958
vinny <vtrolia@protonmail.com>
2019-11-30 15:56:11 -0600

    most current running version for new update

--- laststand/laststand/settings.py
+++ laststand/laststand/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ""
 
-HOME_DIR = "/Users/vinny/Desktop/Documents/"
+HOME_DIR = "/usr/local/www/"
 
 with open(HOME_DIR + '/Last-Stand-Website/laststand/secret_key.txt', "r") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ADMINS = [("Vincent Trolia", "vtrolia@protonmail.com")]
 
 ALLOWED_HOSTS = ['*']
