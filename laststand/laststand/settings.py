c0fec3b49ecf803c9bd6aa3a22b27005f6a92d33
vinny <vtrolia@protonmail.com>
2019-12-31 04:57:49 -0600

    ACTUAL production settings

--- laststand/laststand/settings.py
+++ laststand/laststand/settings.py
@@ -25,7 +25,7 @@ with open(HOME_DIR + '/Last-Stand-Website/laststand/secret_key.txt', "r") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ADMINS = [("Vincent Trolia", "vtrolia@protonmail.com")]
 
 ALLOWED_HOSTS = ['*']
